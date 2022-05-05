372b70f3541287fca85cf09719761f7a6df31464
ababoum <mababou@student.42.fr>
2022-05-05 18:02:53 +0200

    Init vectors

--- vector.hpp
+++ vector.hpp
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   Vector.hpp                                         :+:      :+:    :+:   */
+/*   vector.hpp                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mababou <mababou@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/14 14:06:48 by mababou           #+#    #+#             */
-/*   Updated: 2022/04/14 14:28:02 by mababou          ###   ########.fr       */
+/*   Updated: 2022/05/05 18:01:04 by mababou          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,27 +16,42 @@
 # include <iostream>
 # include <string>
 
+namespace ft {
 template<
     class T,
     class Allocator = std::allocator<T>
 >
-class Vector
+class vector
 {
-
 	public:
 
-		Vector();
-		Vector( Vector const & src );
-		~Vector();
+		typedef	T			value_type;
+		typedef	Allocator	allocator_type;
+		typedef size_type	std::size_t;
 
-		Vector &		operator=( Vector const & rhs );
 
+		
+		vector();
+		explicit vector( const Allocator& alloc );
+		explicit vector( size_type count,
+			const T& value = T(),
+			const Allocator& alloc = Allocator());
+		explicit vector( size_type count );
+
+
+
+		/* CAPACITY */
+		size_type size() const;
 		
 
 	private:
-
+		T*			_array;
+		size_type	_size;
 };
 
-std::ostream &			operator<<( std::ostream & o, Vector const & i );
+std::ostream &			operator<<( std::ostream & o, vector const & i );
+}
+
+#include "vector.tpp"
 
 #endif /* ********************************************************** VECTOR_H */
